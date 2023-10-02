f0ff11c15551668e234d44a3322f9c2698116d23
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-10-02 19:06:54 +0900

    algo

--- boj/Silver/21736.py
+++ boj/Silver/21736.py
@@ -8,15 +8,31 @@ queue = deque()
 for i in range(n):
     t = input().rstrip()
     if "I" in t:
-        queue.append((t.index("I"),i))
-    
+        queue.append((t.index("I"), i))
+
     arr.append(list(t))
 
-dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+visited = [[False] * m for _ in range(n)]
 
+dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+ans = 0
 while queue:
     x, y = queue.popleft()
+
+    if visited[y][x]:
+        continue
+
+    if arr[y][x] == "P":
+        ans += 1
+
+    visited[y][x] = True
+
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
 
-        if 0 <= nx < m and 0 <= ny < n and 
+        if 0 <= nx < m and 0 <= ny < n and not visited[ny][nx] and arr[ny][nx] != "X":
+
+            queue.append((nx, ny))
+
+
+print(ans if ans != 0 else "TT")

a1a763ebdeb869abd6687749126c8e4d1394bc5e
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-10-10 07:04:53 +0900

    algo

---
+++ boj/Gold/14502.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [
+    list(map(int, input().split())) for _ in range(n)
+]
+
+# 1위치 브루트포스
+for i in range()
--- boj/Silver/temp.py
+++ boj/Silver/temp.py
@@ -1,4 +1,6 @@
 a, b, v = map(int, input().split())
 
-t = (v - a) // (a - b) + 1
-print(t + 1)
+if (v - a) % (a - b) == 0:
+    print((v - a) // (a - b) + 1)
+else:
+    print((v - a) // (a - b) + 2)

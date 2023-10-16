4a28f3754c0d4e003b382c326a78337ef288f793
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-10-17 08:49:55 +0900

    algo

--- boj/Gold/12865.py
+++ boj/Gold/12865.py
@@ -2,34 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-arr = []
-d = {}
+table = [0] * (k + 1)
 
 for _ in range(n):
-    w, v = map(int, input().split())
-    arr.append((w, v))
-
-arr.sort()
-
-# 포인터 두 개
-# 만약 k보다 커지면 break
-# 무게 더한 값을 딕셔너리로 관리. (try, except)
-
-for i in range(n):
-    for j in range(i, n):
-        if arr[i][0] + arr[j][0] > k:
-            break
-
-        if i == j:
-            try:
-                d[arr[i][0]] = max(d[arr[i][0]], arr[i][1])
-            except:
-                d[arr[i][0]] = arr[i][1]
-        else:
-            try:
-                d[arr[i][0] + arr[j][0]
-                  ] = max(d[arr[i][0] + arr[j][0]], arr[i][1] + arr[j][1])
-            except:
-                d[arr[i][0] + arr[j][0]] = arr[i][1] + arr[j][1]
-
-print(max(d.values()))
+    w, v = [int(x) for x in input().split()]
+    for j in range(k, 0, -1):
+        if j + w <= k and table[j] != 0:
+            table[j + w] = max(table[j + w], table[j] + v)
+        table[w] = max(table[w], v)
+    print(max(table))

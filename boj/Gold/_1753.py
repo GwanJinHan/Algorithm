e84fa9e6175338335806cc06ba8b0e80001304e1
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-10-23 20:39:16 +0900

    algo

---
+++ boj/Gold/_1753.py
@@ -0,0 +1,17 @@
+import sys
+from heapq import heappop
+input = sys.stdin.readline
+
+v, e = map(int, input().split())
+start = int(input())
+
+matrix = [[sys.maxsize] * v for _ in range(v)]
+visit = [True] + [False] * (v - 1)
+
+for i in range(v):
+    matrix[i][i] = 0
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    matrix[a - 1][b - 1] = c
+    matrix[b - 1][a - 1] = c
